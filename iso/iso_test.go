0597d2aa9aa6985e10aed8e378968c4ca125a020
Tor Borgen <Tor Borgen>
2017-03-01 16:08:01 +0100

    update test

--- iso/iso_test.go
+++ iso/iso_test.go
@@ -1,10 +1,21 @@
 package iso
 
+import (
+	"testing"
+)
+
+func TestGretingsExtendedASCII(t *testing.T) {
+	ASCII := GreetingExtendedASCII2()
+	if !(isASCII(ASCII)) {
+		t.Fail()
+	}
+}
+
 func isASCII(s string) bool {
 	for _, C := range s {
-		if C > 127 {
-			return false
+		if C > 127 && C < 256 {
+			return true
 		}
 	}
-	return true
+	return false
 }
