0366340222f5a8e2fb0f781fc603fe2422cad4d5
asalmu16 <asalmu16@student.uia.no>
2017-03-02 15:40:34 +0100

    LÃ¸snning oppg.3 a og endringer -Ali

---
+++ main_lang.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"./lang"
+)
+
+func main() {
+
+	lang.Lang()
+	fmt.Println(lang.Lang())
+}
---
+++ main_riddle.go
@@ -0,0 +1,8 @@
+package main
+
+import "./riddle"
+
+func main() {
+	riddle.RiddleASCII()
+
+}
