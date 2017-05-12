7873230c4f66e5b68d8e0a5dedcfbd60121f3f42
Tor Borgen <Tor Borgen>
2017-05-12 14:51:20 +0200

    Relative paths to the fileutils pack and corrected ASCII to spec

--- ascii/ascii.go
+++ ascii/ascii.go
@@ -2,19 +2,11 @@ package ascii
 
 import "fmt"
 
-/*
-ASCII ... has chars
-*/
-const ASCII = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" +
-	"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f" +
-	` !"#$%&'()*+,-./0123456789:;<=>?` +
-	`@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_` +
-	"`abcdefghijklmnopqrstuvwxyz{|}~\x7f"
 
 /*
 IterateOverASCIIStringLiteral does the interating
 */
-func IterateOverASCIIStringLiteral() {
+func IterateOverASCIIStringLiteral(ASCII string) {
 
 	for i := 0; i < len(ASCII); i++ {
 		fmt.Printf("%X %+q %b\n", ASCII[i], ASCII[i], ASCII[i])
